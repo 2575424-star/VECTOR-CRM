65cafad4ed7bec319cbc8dda5683d118724fa408
2575424-star <2575424@gmail.com>
2026-09-10 11:31:47 +0300

    Android v1.1.1: bump app version

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "ru.vectorcrm.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.1.1"
     }
 
     compileOptions {
